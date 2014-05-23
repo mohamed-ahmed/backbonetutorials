6f224fc0a2c21a394b7030300fa895a9a453d5d2
mohamahmed <mohamahmed@blackberry.com>
2014-05-23 17:15:52 -0400

    created Utils.js to make code more modular

--- examples/modular-backbone/js/models/AppModel.js
+++ examples/modular-backbone/js/models/AppModel.js
@@ -1,39 +1,19 @@
 define([
 	'underscore',
-	'backbone'
-	], function(_, Backbone) {
+	'backbone',
+	'Utils'
+	], function(_, Backbone, Utils) {
 
 		var AppModel = Backbone.Model.extend({
 
 			initialize: function(){
-				this.getHTML();
-			},
-
-			getHTML : function(){
-				var siteUrl = this.attributes.url;
-				console.log(siteUrl);
-				var html;
-				var xhr = new XMLHttpRequest();
-				xhr.open("GET", siteUrl, true);
-				xhr.onreadystatechange = function() {
-					if (xhr.readyState == 4) {
-						// JSON.parse does not evaluate the attacker's scripts.
-						var resp = JSON.parse(xhr.responseText);
-						//console.log(xhr.responseText);
-
-						html = $(xhr.responseText);
-						console.log("html: ");
-						console.log(html);
-						//console.log(domObj);
-					}
-				}
-				xhr.send();
+				Utils.getHTML(this.attributes.url, function (html){ console.log(); });
 			}
 
 
 
+		});
+
+	return AppModel;
+
 });
-
-		return AppModel;
-
-	});
