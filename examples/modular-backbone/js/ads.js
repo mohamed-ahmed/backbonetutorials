8f046030db3ceb6ed8e2d63431e9f49da436f2f0
Mohamed Ahmed <mohamed.ahmed@live.ca>
2014-07-10 00:06:44 -0400

    testing ads.js

--- examples/modular-backbone/js/ads.js
+++ examples/modular-backbone/js/ads.js
@@ -1,4 +1,5 @@
   ( function() {
+  	console.log("\n\nin ad.js \n\n");
     if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
     var unit = {"calltype":"async[2]","publisher":"mohamedahmed0071","width":550,"height":250,"sid":"Chitika Default"};
     var placement_id = window.CHITIKA.units.length;
